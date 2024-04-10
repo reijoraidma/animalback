851164493e3070421afe64943c770f137f60f55c
fredm <fred.mihkelsoo@gmail.com>
2024-04-11 01:32:58 +0300

    Created test for /login rest controller

--- src/main/java/com/lostfound/animalback/business/login/LoginController.java
+++ src/main/java/com/lostfound/animalback/business/login/LoginController.java
@@ -5,12 +5,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class LoginController {
     private LoginService loginService;
 
     @GetMapping("/login")
-    public LoginResponse login(@RequestParam String username, @RequestParam String password) {
-        return loginService.login(username, password);
+    //public LoginResponse login(@RequestParam String username, @RequestParam String password) {
+    public List<String> login(@RequestParam String username, @RequestParam String password) {
+        //return loginService.login(username, password);
+        return List.of(username,password);
+
     }
 }
