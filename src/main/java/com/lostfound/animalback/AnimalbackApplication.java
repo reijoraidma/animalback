0717444e810b1e319a5392f56a7b54b802272a16
Reijo <reijo.raidma@gmail.com>
2024-04-10 11:03:04 +0300

    Initial commit

---
+++ src/main/java/com/lostfound/animalback/AnimalbackApplication.java
@@ -0,0 +1,13 @@
+package com.lostfound.animalback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalbackApplication.class, args);
+	}
+
+}
