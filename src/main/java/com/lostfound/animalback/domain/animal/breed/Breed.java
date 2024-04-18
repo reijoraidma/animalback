c4b2efe6c575c131e543a361a53b18a901ebdfae
fredm <fred.mihkelsoo@gmail.com>
2024-04-18 23:34:56 +0300

    Small fix

--- src/main/java/com/lostfound/animalback/domain/animal/breed/Breed.java
+++ src/main/java/com/lostfound/animalback/domain/animal/breed/Breed.java
@@ -1,6 +1,6 @@
 package com.lostfound.animalback.domain.animal.breed;
 
-import com.lostfound.animalback.domain.AnimalType;
+import com.lostfound.animalback.domain.animal.animaltype.AnimalType;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
