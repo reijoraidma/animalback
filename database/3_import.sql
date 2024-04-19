56446b648225b4086d6a7cd46025760721b0fd3c
Reijo <reijo.raidma@gmail.com>
2024-04-19 09:34:22 +0300

    Fixed db inputs

--- database/3_import.sql
+++ database/3_import.sql
@@ -11,10 +11,10 @@ INSERT INTO animal.animal_type (id, name, image_data, status) VALUES (default, '
 INSERT INTO animal.animal_type (id, name, image_data, status) VALUES (default, 'Kass', 'AAAAAA', 'A');
 INSERT INTO animal.animal_type (id, name, image_data, status) VALUES (default, 'Madu', 'AAAA', 'A');
 
-INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (1, 'Taks', 1, 'A');
-INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (2, 'Siiam', 2, 'A');
-INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (3, 'Nastik', 3, 'A');
-INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (4, 'SaksaLamba', 1, 'A');
-INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (5, 'Kolli', 1, 'A');
-INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (6, 'KuriMuri', 1, 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (default, 'Taks', 1, 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (default, 'Siiam', 2, 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (default, 'Nastik', 3, 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (default, 'SaksaLamba', 1, 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (default, 'Kolli', 1, 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, status) VALUES (default, 'KuriMuri', 1, 'A');
 
