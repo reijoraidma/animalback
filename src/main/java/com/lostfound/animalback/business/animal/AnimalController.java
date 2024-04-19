53d7cb929c39c1b969d404d8ed7f1b9d761b1541
Reijo <reijo.raidma@gmail.com>
2024-04-19 14:35:19 +0300

    Animal controller

--- src/main/java/com/lostfound/animalback/business/animal/AnimalController.java
+++ src/main/java/com/lostfound/animalback/business/animal/AnimalController.java
@@ -1,16 +1,11 @@
 package com.lostfound.animalback.business.animal;
 
-import com.lostfound.animalback.business.animal.breed.dto.BreedResponse;
 import com.lostfound.animalback.business.animal.dto.AnimalInfo;
-import com.lostfound.animalback.domain.Animal;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -19,12 +14,9 @@ public class AnimalController {
     private AnimalService animalService;
 
 
-
     @Operation(summary = "Tagastab Looma info")
     @GetMapping("/animal/{animalId}")
     public AnimalInfo getAnimal(@PathVariable Integer animalId) {
         return animalService.getAnimal(animalId);
     }
-
-
 }
