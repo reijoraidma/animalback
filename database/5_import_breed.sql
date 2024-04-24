eb7f81dde4498439868ce698b11ec2a0f9024d5c
Reijo <reijo.raidma@gmail.com>
2024-04-24 13:34:58 +0300

    Updated Imports to breed, animal, post

--- database/5_import_breed.sql
+++ database/5_import_breed.sql
@@ -1,6 +1,8 @@
-INSERT INTO animal.breed (id, type, animal_type_id,image_data, status) VALUES (default, 'Taks', 1, 'AAAAAA', 'A');
-INSERT INTO animal.breed (id, type, animal_type_id,image_data, status) VALUES (default, 'Siiam', 2, 'AAAAAA', 'A');
-INSERT INTO animal.breed (id, type, animal_type_id,image_data, status) VALUES (default, 'Nastik', 3, 'AAAAAA', 'A');
-INSERT INTO animal.breed (id, type, animal_type_id,image_data, status) VALUES (default, 'SaksaLamba', 1, 'AAAAAA', 'A');
-INSERT INTO animal.breed (id, type, animal_type_id,image_data, status) VALUES (default, 'Kolli', 1, 'AAAAAA', 'A');
-INSERT INTO animal.breed (id, type, animal_type_id,image_data, status) VALUES (default, 'KuriMuri', 1, 'AAAAAA', 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, image_data, status) VALUES (default, 'Taks', 1, 'AAAAAA', 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, image_data, status) VALUES (default, 'Siiam', 2, 'AAAAAA', 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, image_data, status) VALUES (default, 'Nastik', 3, 'AAAAAA', 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, image_data, status) VALUES (default, 'SaksaLamba', 1, 'AAAAAA', 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, image_data, status) VALUES (default, 'Kolli', 1, 'AAAAAA', 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, image_data, status) VALUES (default, 'KuriMuri', 1, 'AAAAAA', 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, image_data, status) VALUES (default, 'Bengal', 2, 'AAAAAA', 'A');
+INSERT INTO animal.breed (id, type, animal_type_id, image_data, status) VALUES (default, 'Maine Coon', 2, 'AAAAAA', 'A');
