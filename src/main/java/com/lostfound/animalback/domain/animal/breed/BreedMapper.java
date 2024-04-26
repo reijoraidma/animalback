5ae761bb99863605ee8ac34f433a2ea6b3c9cae7
Reijo <reijo.raidma@gmail.com>
2024-04-26 17:05:47 +0300

    Changed BreedInfo and breedmapper

--- src/main/java/com/lostfound/animalback/domain/animal/breed/BreedMapper.java
+++ src/main/java/com/lostfound/animalback/domain/animal/breed/BreedMapper.java
@@ -4,15 +4,17 @@ import com.lostfound.animalback.business.Status;
 import com.lostfound.animalback.business.animal.breed.dto.BreedInfo;
 import com.lostfound.animalback.business.animal.breed.dto.BreedRequest;
 import org.mapstruct.*;
+import util.StringConverter;
 
 import java.util.List;
 
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = MappingConstants.ComponentModel.SPRING,imports = {StringConverter.class})
 public interface BreedMapper {
 
     @Named("toBreedResponse")
     @Mapping(source = "type", target = "breedName")
     @Mapping(source = "id", target = "breedId")
+    @Mapping(expression = "java(StringConverter.bytesToString(breed.getImageData()))", target = "breedImageData")
     BreedInfo toBreedInfo(Breed breed);
 
     @IterableMapping(qualifiedByName = "toBreedResponse")
