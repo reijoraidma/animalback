83b8e0e0322cd5ee5234ec55d106ffb406ac8d13
fredm <fred.mihkelsoo@gmail.com>
2024-04-29 11:01:53 +0300

    Operation description for swagger for animal images endpoints

--- src/main/java/com/lostfound/animalback/business/animal/animalimage/AnimalImageController.java
+++ src/main/java/com/lostfound/animalback/business/animal/animalimage/AnimalImageController.java
@@ -1,6 +1,8 @@
 package com.lostfound.animalback.business.animal.animalimage;
 
 import com.lostfound.animalback.business.animal.animalimage.dto.AnimalImageSave;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,11 +20,15 @@ public class AnimalImageController {
     private final AnimalImageService animalImageService;
 
     @PostMapping("/animals/{animalId}/animalImages")
+    @Operation(summary = "Save animal images", description = "Save a list of animal images imageData by animal id")
+    @ApiResponse(responseCode = "200", description = "OK")
     public void saveAnimalImages(@PathVariable Integer animalId, @RequestBody List<AnimalImageSave> animalImages) {
         animalImageService.saveAnimalImages(animalId, animalImages);
     }
 
     @GetMapping("/animal/images")
+    @Operation(summary = "Get animal images", description = "Get a list of animal images imageData by animal id")
+    @ApiResponse(responseCode = "200", description = "OK")
     public List<AnimalImageInfo> getAnimalImageInfos(@RequestParam Integer animalId) {
         return animalImageService.getAnimalImageInfos(animalId);
     }
